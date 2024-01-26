9c509e61ab4a92391e600dfad8107937d1234a44
MeetSondagar <meetsondagar030905@gmail.com>
2024-01-26 20:40:36 +0530

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,116 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*=============== HOME SWIPER ===============*/
+let homeSwiper = new Swiper(".home-swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  spaceBetween: 30,
+  loop: true,
+  autoplay: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 50) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+/*=============== NEW SWIPER ===============*/
+window.addEventListener("scroll", scrollHeader);
+
+let newSwiper = new Swiper(".new-swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  centeredSlides: true,
+  slidesPerView: "auto",
+  spaceBetween: 15,
+  loop: true,
+  autoplay: true,
+});
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 460 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 460) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 1500,
+  reset: true,
+});
+
+sr.reveal(".header");
+sr.reveal(".home-swiper");
+sr.reveal(".about");
+sr.reveal(".trick");
+sr.reveal(".category");
+sr.reveal(".discount");
+sr.reveal(".new");
+sr.reveal(".newsletter");
+sr.reveal(".footer");
